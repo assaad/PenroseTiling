582ef76248daec2d0d0a25b2c2ce0e5f98916c65
Assaad Moawad <assaad.moawad@datathings.com>
2020-11-21 08:25:39 +0100

    update

--- src/main/java/PenroseTiling.java
+++ src/main/java/PenroseTiling.java
@@ -1,55 +1,50 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class PenroseTiling extends JPanel {
-    private final JFrame f;
-    /**
-     * Constructor for objects of class Reveal
-     */
-    public PenroseTiling(String name) {
-        f = new JFrame();
-        f.setTitle(name);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setVisible(true);
-
-        //Sets the size of the window
-        f.setSize(1500, 800);
-        f.setResizable(false);
+public class PenroseTiling {
+    private static JFrame frame;
+    private static JPanel leftPanel;
+    private static JPanel picturePanel;
 
 
-        /*Creating and setting contentPane Properties*/
-        JPanel contentPane = new JPanel(new FlowLayout(FlowLayout.RIGHT, 2, 2));
-        /*Creating and setting drawing panel Properties*/
-        JPanel drawing = new JPanel();
-        drawing.setPreferredSize(new Dimension(1240, 800));
-        drawing.setBackground(new Color(240, 240, 255));
 
-        /*Creating and setting right panel Properties*/
-        JPanel rightPan = new JPanel(null);
-        rightPan.setPreferredSize(new Dimension(250, 800));
-        rightPan.setBackground(Color.GRAY);
-
-        /*Add panels to contentPane*/
-        contentPane.add(drawing);
-        contentPane.add(rightPan);
-
-
-        /*Setting content panel on the frame*/
-        f.setContentPane(contentPane);
-        setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
+    public static void main(String[] args) throws Exception{
+        SwingUtilities.invokeAndWait(new Runnable(){
             @Override
             public void run() {
-                try {
-                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-                new PenroseTiling("Penrose Tiling");
+                final int MINIMUM = 600;
+                frame = new JFrame();
+                frame.add(new JToolBar(), BorderLayout.NORTH);
+
+                leftPanel = new JPanel();
+                picturePanel = new JPanel();
+
+                //Create a split pane with the two scroll panes in it.
+                JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+                        leftPanel, picturePanel);
+                splitPane.setOneTouchExpandable(false);
+                splitPane.setDividerLocation(250);
+
+                picturePanel.setBackground(Color.green);
+                picturePanel.setOpaque(true);
+                picturePanel.setPreferredSize(new Dimension(1250, 800));//hint at size
+                picturePanel.setMinimumSize(new Dimension(550, 600));
+
+                leftPanel.setBackground(Color.cyan);
+                leftPanel.setOpaque(true);
+                leftPanel.setPreferredSize(new Dimension(250, 800));//hint at size
+                leftPanel.setMinimumSize(new Dimension(250, 800));//hint at size
+
+                frame.setMinimumSize(new Dimension(800, 600));
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setTitle("Penrose Tiling");
+                frame.add(splitPane);
+
+                frame.pack();
+                frame.setSize(1500, 800);
+                frame.setVisible(true);
             }
+
         });
     }
 
